c796c75bdab2138721b9df01b8a24010694d44e1
GueldenBayar <81645801+GueldenBayar@users.noreply.github.com>
2021-05-28 17:02:22 +0200

    implemented some API and react components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios'; 
+import Header from './components/Header.js';
+import Newscontainer from './components/Newscontainer.js';
+import Footer from './components/Footer.js';
+import Searchbar from './components/Searchbar.js'
+import PacmanLoader from 'react-spinners/PacmanLoader';
+import {css} from '@emotion/react'
+
+const emo = css`
+position: absolute;
+top: 50%;
+right: 50%;
+transform: translate (50%, -50%);
+`
+
+
 
 function App() {
+//  const url = 'http://hn.algolia.com/api/v1/search?tags=front_page';
+const searchTopic= (topic) => {
+  axios({
+    //get by default
+  method: 'get',
+  url: `http://hn.algolia.com/api/v1/search?query=${topic}&tags=story`
+})  
+  .then(function (response) { 
+    console.log(`for topic: ${topic}\n`+ JSON.stringify(response))
+  })
+} 
+searchTopic('react');
+
+axios({
+    method: 'get',
+    url: 'http://hn.algolia.com/api/v1/search?tags=front_page'
+  })
+    .then(function (response) {
+      console.log(response)
+    });
+
+
+
+    
+    
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+{/*     <PacmanLoader css={emo} size={100} color={'purple'} speedMultiplier={3} />
+ */}    
+     <Header />
+     <Newscontainer />
+     <Footer /> 
     </div>
   );
 }
